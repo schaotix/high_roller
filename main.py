659d9f79d470d161fab5e8443a26eb0eeb943e38
screamer <screamer.chaotix.24@gmail.com>
2015-04-03 14:51:16 -0400

    game over

--- main.py
+++ main.py
@@ -68,32 +68,58 @@ def six():
     -----------
     ''')
 
-print("Greetings and Welcome to High Roller.")
-time.sleep(1)
-name_one = input("Player One, please enter your name: ")
-time.sleep(1)
-name_two = input("Player Two, please enter your name: ")
-time.sleep(1)
-print("Prepare to duel, {} and {}.".format(name_one, name_two))
-go = input("Press Enter to roll, {}".format(name_one))
 
-roll_one = random.randrange(1,6)
+def roller(r):
+    if r == 1:
+        one()
+    elif r == 2:
+        two()
+    elif r == 3:
+        three()
+    elif r == 4:
+        four()
+    elif r == 5:
+        five()
+    elif r == 6:
+        six()
+    else:
+        print('Ya done broke it!')
 
-if roll_one == 1:
-    one()
-elif roll_one == 2:
-    two()
-elif roll_one == 3:
-    three()
-elif roll_one == 4:
-    four()
-elif roll_one == 5:
-    five()
-elif roll_one == 6:
-    six()
-else:
-    print("Whoa, what did you roll?")
-    
+def game():    
+    input('{}, press Enter to roll.'.format(name_one))
+    roll_one = random.randrange(1,7)
+    roller(roll_one)
+    time.sleep(1)
+    input('{}, press Enter to roll'.format(name_two))
+    roll_two = random.randrange(1,6)
+    roller(roll_two)
+    time.sleep(2)
 
-class Player_One(self):
+    if roll_one > roll_two:
+        print('{} Wins.'.format(name_one))
+    elif roll_one < roll_two:
+        print('{} Wins.'.format(name_two))
+    else:
+        print('Tie!')
 
+    again = input('Play again? ')
+    again.islower()
+    if again in ('yes', 'ye', 'y', 'yeah', 'yep', 'ya', 'yea'):
+        game()
+    else:
+        print('Thanks for playing. Buhbye.')
+
+def main():
+    global name_one
+    global name_two
+    print("Greetings and Welcome to High Roller.")
+    time.sleep(1)
+    name_one = input("Player One, please enter your name: ")
+    time.sleep(1)
+    name_two = input("Player Two, please enter your name: ")
+    time.sleep(1)
+    print("Prepare to duel, {} and {}.".format(name_one, name_two))
+    time.sleep(1)
+    game()
+
+main()
